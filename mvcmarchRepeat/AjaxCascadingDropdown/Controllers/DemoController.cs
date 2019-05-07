db22656a666ac6d639888a9dfc4eddce481252b7
mktrptn <mktr@gmail.com>
2019-05-07 22:43:58 +0530

    a

---
+++ mvcmarchRepeat/AjaxCascadingDropdown/Controllers/DemoController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AjaxCascadingDropdown.Models;
+
+namespace AjaxCascadingDropdown.Controllers
+{
+    public class DemoController : Controller
+    {
+        InventoryEntities obj = new InventoryEntities();
+       
+        public ActionResult Index()
+        {
+            List<SelectListItem> cat = new List<SelectListItem>();
+            cat.Add(new SelectListItem { Text="-Select Category-"});
+            foreach(var c in obj.categories.ToList() )
+            {
+                cat.Add(new SelectListItem { Text=c.catedesc , Value=c.cateid.ToString() });
+            }
+            ViewBag.category = cat;
+
+            return View();
+        }
+
+        public JsonResult getproducts(int cid)
+        {
+            var prod = from p in obj.products where p.cateid == cid select new { p.proddesc, p.prodid };
+            return Json(prod , JsonRequestBehavior.AllowGet);
+        }
+
+
+    }
+}
---
+++ mvcmarchRepeat/AjaxCascadingDropdown/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AjaxCascadingDropdown.MvcApplication" Language="C#" %>
